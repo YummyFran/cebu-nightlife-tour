a35646c7b37eb4be5e3f4a4885c5b5fd6742ac7c
YummyFran <yumpalomares@gmail.com>
2026-03-08 19:21:09 +0800

    meta

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
                 <li><NavLink to={'/page-2'} className={({ isActive }) => isActive ? 'active' : ''}>필리핀사람들의 성격 및 특징</NavLink></li>
                 <li><NavLink to={'/page-3'} className={({ isActive }) => isActive ? 'active' : ''}>필리핀에서 만난 한글들</NavLink></li>
             </ul>
-            <div className='logo'>세부밤문화투어•세부황제투어•세부에코걸</div>
+            <h1 className='logo'>세부밤문화투어•세부황제투어•세부에코걸</h1>
         </div>
     </nav>
   )
