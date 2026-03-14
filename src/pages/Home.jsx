563bef806bcf30abae197105dd5890ca6c8f76e0
YummyFran <yumpalomares@gmail.com>
2026-03-14 15:03:07 +0800

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
           <div className='wrapper'>
             <h2>세부에코걸 정보를 찾는 중이신가요?</h2>
             
-            <div>
+            <div className='hero'>
               <p className='subtitle'>아래 메인홈페이지 링크(사진)클릭하시고 <br /> 더 많은 세부밤문화 정보 얻어가세요</p>
               <a href="https://www.cebunightguide.com" target='_blank'>
                 <img src={`${isMobile ? '/banner1.0.jpg' : '/banner.png'}`} alt="banner" className={`banner clickable ${isMobile ? "mobile" : ""}`} />
