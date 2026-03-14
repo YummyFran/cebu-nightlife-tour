ae2577f2a3cc45c74ff9db1d0b5be54976634be0
YummyFran <yumpalomares@gmail.com>
2026-03-14 21:10:17 +0800

    changes

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -11,12 +11,12 @@ const Page3 = () => {
           <p className='line'>동남아의 많은 국가들에서 처럼</p>
           <p className='line'>필리핀에도 한류 문화가 깊숙이 자리 잡고 있습니다</p>
           <p className='line'>​K-POP이 익숙한 그들의 레퍼터리이기도 하고</p>
-          <p className='line'>K-Pop을 주제로 하는 유명 웹사이트가 <br />성행하고 있으며</p>
-          <p className='line'>K-Pop을 주제로 한 특정 페이스북에는 팔로어가 30만을 넘어가기도 합니다</p>
+          <p className='line'>K-Pop을 주제로 하는 웹사이트가 성행하고 있으며</p>
+          <p className='line'>K-Pop을 주제로 한 특정 페이스북에는 팔로어가 <br />30만을 넘어가기도 합니다</p>
 
           <img src="/banner6.jpg" alt="banner6" className='banner' />
 
-          <p className='line'>배우 이민호나 가수 김범수는 20대를 넘어 <br />30,40대의 </p>
+          <p className='line'>가수 김범수는 20대를 넘어 30,40대의 </p>
           <p className='line'>필리피노들이 매우 잘 알고 있으며 김범수의 노래를</p>
           <p className='line'>따라 부르기도 할 정도입니다</p>
           <p className='line'>그리고 필리핀에서는 일본의 TV보다</p>
@@ -44,7 +44,7 @@ const Page3 = () => {
           <p className="line">정확히 말하면 의미를 모르고 디자인과 한글이라는 </p>
           <p className="line">이유로 입는다는 표현미 맞을 것 같습니다</p>
           <p className="line">아래는 필리핀에서 만난 한국 옷을 입은 <br />사람들의 모습입니다</p>
-          <p className="line">필리핀에 살면서 만난 사람들의 모습을 담아 보관해 놓았던 자료들입니다</p>
+          <p className="line">필리핀에 살면서 만난 사람들의 모습을 <br />담아 보관해 놓았던 자료들입니다</p>
 
           <img src="/banner9.jpg" alt="banner9" className='banner' />
 
@@ -55,7 +55,7 @@ const Page3 = () => {
           <p className="line">23기 기수 후보생이라고 쓰여있군요 </p>
           <p className="line">아마도 경마의 기수 훈련생들이 <br />입던 옷인 것 같습니다</p>
           <p className="line">저 옷이 어떡해 과천 경마장에서부터 </p>
-          <p className="line">필리핀의 외진 동네까지 흘러갈 수 있었는지 궁금하기도 합니다..</p>
+          <p className="line">필리핀의 외진 동네까지 흘러갈 수 있었는지 <br />궁금하기도 합니다..</p>
 
           <img src="/banner10.jpg" alt="banner10" className='banner' />
 
@@ -65,13 +65,13 @@ const Page3 = () => {
           <p className="line">삼룡은 용이 새마리라는 뜻이지만.....</p>
           <p className="line">번역기로 용세마리를 번역한거같네요</p>
           <p className="line">삼룡이라는 의미가 한국에서는 <br />어떻게 쓰이는지 알면</p>
-          <p className="line">아마도 문신을 지우고 후회의 눈물을 흘리지  않을까 생각합니다</p>
+          <p className="line">아마도 문신을 지우고 후회의 눈물을 <br />흘리지  않을까 생각합니다</p>
 
           <img src="/banner11.jpg" alt="banner11" className='banner' />
 
           <p className="line">맥도날드에서 만난 세부 한식당 고깃집의 직원</p>
           <p className="line">유니폼에 한국 국기가 새겨져 있습니다</p>
-          <p className="line">주인은 한국 아이덴티티 마케팅을 아주 잘 하고 있는 것 같습니다.</p>
+          <p className="line">주인은 한국 아이덴티티 마케팅을 <br />아주 잘 하고 있는 것 같습니다.</p>
 
           <img src="/banner12.jpg" alt="banner12" className='banner' />
 
@@ -79,7 +79,7 @@ const Page3 = () => {
           <p className="line">세부의 길바닥을 달리고 있을까요 </p>
           <p className="line">혹시 경희대 오토바이 국가대표 선수가 </p>
           <p className="line">몰래 필리핀에 전지훈련을 온 것은 아닐까요.. </p>
-          <p className="line">세부의 지옥 같은 교통 환경이 혹시 기술 연마에 도움이 돼서는 아닐까요 .. </p>
+          <p className="line">세부의 지옥 같은 교통 환경이 혹시 <br />기술 연마에 도움이 돼서는 아닐까요 .. </p>
           <p className="line">​세상에는 미스터리 한 일들이 너무 많습니다.  ㅋㅋ</p>
 
         </div>
